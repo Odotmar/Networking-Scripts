8a7fd5bd3e84917170ac34cc355f034be8743fbe
Odotmar <75050679+Odotmar@users.noreply.github.com>
2021-10-24 03:31:37 +0100

    Update EndUserTest.py
    Making more changes

--- EndUserTest.py
+++ EndUserTest.py
@@ -3,5 +3,18 @@
 import subprocess
 import sys
 
+#Sets location for files
+path_for_output = "~/Desktop/Sohonet-Triage.txt"
 
-subprocess.run(["ls", "-l"])
+#Get customer public IP
+cust_pub_ip = subprocess.run(["curl ifconfig.me"])
+#Get OS Info
+cust_os = subprocess.run(["/ etc / os - release | grep PRETTY "])
+
+myoutput = open(path_for_output,'w+')
+
+p = Popen([cust_os,cust_pub_ip], stdout=myoutput, stderr=subprocess.PIPE, universal_newlines=True)
+
+
+with open(path_for_output,"r") as f:
+    print(f.read())
